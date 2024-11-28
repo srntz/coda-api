080bf528e2b86f202553768a26465e4682e0616f
s-raniytuz <tuzsocial909@gmail.com>
2024-11-27 23:07:25 -0700

    Add album inference logic and change available playlist IDs

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -1,10 +1,19 @@
+// const playlists: string[] = [
+//   "37i9dQZF1DX0MLFaUdXnjA", // chill pop
+//   "37i9dQZF1DX889U0CL85jj", // chill vibes
+//   "37i9dQZF1DX6tTW0xDxScH", // stay in bed
+//   "37i9dQZF1DXcBWIGoYBM5M", // today's top hits
+//   "37i9dQZF1DX7XNgsy4UFju", // indie all stars
+//   "37i9dQZF1DX4OzrY981I1W" // my life is a movie
+// ]
+
 const playlists: string[] = [
-  "37i9dQZF1DX0MLFaUdXnjA", // chill pop
-  "37i9dQZF1DX889U0CL85jj", // chill vibes
-  "37i9dQZF1DX6tTW0xDxScH", // stay in bed
-  "37i9dQZF1DXcBWIGoYBM5M", // today's top hits
-  "37i9dQZF1DX7XNgsy4UFju", // indie all stars
-  "37i9dQZF1DX4OzrY981I1W" // my life is a movie
+  "3kzid03r3MfxYOCY0JIt7K", // mobile grind
+  "0FYLBXJnBqHVmQaroHk2ps", // zakid vkid
+  "7et1kGmJxaqRzERUSRQeaf", // ru
+  "6JymepNseqyNBmTtGPueNN", // noviy russkiy klubnyak
+  "5Tw1qO7q4HLQegTMrsmNY8" // sranin
+
 ]
 
 export default playlists;
--- src/services/random-album/randomAlbum.service.ts
+++ src/services/random-album/randomAlbum.service.ts
@@ -1,40 +1,69 @@
 import {IService} from "../../interfaces/IService.js";
-import AlbumObjectFull = SpotifyApi.AlbumObjectFull;
 import playlists from "../../data/playlists.js";
 import {IToken} from "../../interfaces/IToken.js";
 import PlaylistObjectFull = SpotifyApi.PlaylistObjectFull;
 import PlaylistTrackObject = SpotifyApi.PlaylistTrackObject;
 import {ISpotifyFullErrorObject} from "../../types/SpotifyApiUtilityTypes.js";
+import AlbumObjectSimplified = SpotifyApi.AlbumObjectSimplified;
 
+// The number of playlists processed in one generation cycle
 const PLAYLIST_COUNT = 5;
 
-export class RandomAlbumService implements IService<AlbumObjectFull> {
+// The amount of albums that the service returns per call
+const RETURN_ALBUM_COUNT = 5;
+
+// The allowed number of generation iterations before a timeout error gets thrown
+const ALLOWED_GENERATION_ATTEMPTS = 3;
+
+export class RandomAlbumService implements IService<AlbumObjectSimplified[]> {
   private playlistArray: string[];
   private chosenPlaylists: string[] = [];
   private token: IToken
   private tracks: PlaylistTrackObject[] = [];
+  private albums: AlbumObjectSimplified[] = [];
 
   constructor(token: IToken) {
     this.token = token;
-    this.playlistArray = [...playlists]
-    this.determinePlaylistIds()
   }
 
   /**
    * Generates a list of recommended albums based on randomly selected playlist ids
    * @returns List of selected albums
    */
-  async get(): Promise<AlbumObjectFull> {
+  async get(): Promise<AlbumObjectSimplified[]> {
+    const generationAttempts = 0;
+    while(this.albums.length < RETURN_ALBUM_COUNT && generationAttempts <= ALLOWED_GENERATION_ATTEMPTS) {
+      await this.constructAlbumsArray()
+    }
+
+    if(this.albums.length < RETURN_ALBUM_COUNT) {
+      const error: ISpotifyFullErrorObject = {
+        error: {
+          status: 408,
+          message: "Generation timed out.",
+        }
+      }
+
+      throw new Error(JSON.stringify(error))
+    }
+
+    return this.albums;
+  }
+
+  /**
+   * A wrapper on the entire generation logic. Performs calls to functions that
+   * choose playlist ids, construct the tracks array and fill the albums array.
+   * @private
+   */
+  private async constructAlbumsArray() {
+    this.determinePlaylistIds()
+
     for (const id of this.chosenPlaylists) {
       const playlistData = await this.fetchPlaylistData(id)
       this.tracks.push(...playlistData.tracks.items)
     }
 
-    this.tracks.forEach(track => {
-      console.log(track.track.name)
-    })
-
-    return Promise.resolve(undefined);
+    this.inferAlbumsFromTracks()
   }
 
   /**
@@ -43,6 +72,9 @@ export class RandomAlbumService implements IService<AlbumObjectFull> {
    * @private
    */
   private determinePlaylistIds() {
+    this.playlistArray = [...playlists]
+    this.chosenPlaylists = [];
+
     for (let i = 0; i < PLAYLIST_COUNT; i++) {
       const randomIndex = Math.floor(Math.random() * this.playlistArray.length);
       this.chosenPlaylists.push(this.playlistArray[randomIndex]);
@@ -71,5 +103,23 @@ export class RandomAlbumService implements IService<AlbumObjectFull> {
     }
 
     return playlistData as PlaylistObjectFull;
+  }
+
+  /**
+   * Fills the albums array from randomly selected tracks.
+   * @private
+   */
+  private inferAlbumsFromTracks() {
+    while(this.albums.length < RETURN_ALBUM_COUNT && this.tracks.length > 0) {
+      const randomIndex = Math.floor(Math.random() * this.tracks.length)
+      const currentTrack = this.tracks[randomIndex].track;
+      const selectedAlbumsIds = this.albums.map(item => item.id);
+
+      // Add an album to the selected albums array if album_type is "album" and it does not already exist in the selected albums array
+      if(currentTrack.album.album_type === "album" && !selectedAlbumsIds.includes(currentTrack.album.id)) {
+        this.albums.push(currentTrack.album);
+        this.tracks.splice(randomIndex, 1);
+      }
+    }
   }
 }
